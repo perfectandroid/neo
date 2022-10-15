f82740bf9963e0d5f305586600207d3959943b51
Ranjithk <psstechteam@gmail.com>
2022-10-15 10:38:34 +0530

    Dispatched List Implement

--- lib/Screens/Home/home_page.dart
+++ lib/Screens/Home/home_page.dart
@@ -6,6 +6,9 @@ import 'package:neo/Screens/Home/home_activity.dart';
 import 'package:neo/Screens/Login/login_page.dart';
 import 'package:neo/Screens/Register/signup_page.dart';
 import 'package:neo/Screens/Status/confirmlist.dart';
+import 'package:neo/Screens/Status/deliverylist.dart';
+import 'package:neo/Screens/Status/dispatchedlist.dart';
+import 'package:neo/Screens/Status/packedlist.dart';
 import 'package:neo/Screens/Tabs/tab.dart';
 import '../../constants.dart';
 import '../../helper/sharedprefhelper.dart';
@@ -560,35 +563,38 @@ class _HomePageState extends State<HomePage> {
      }
    else if(album.title=="Packed")
    {
-     Fluttertoast.showToast(
-       msg: "Packed",
-       textColor: Colors.white,
-       toastLength: Toast.LENGTH_SHORT,
-       timeInSecForIosWeb: 1,
-       gravity: ToastGravity.BOTTOM,
-       backgroundColor: Colors.indigo,
+     Navigator.push(
+       context,
+       MaterialPageRoute(
+         fullscreenDialog: true,
+         builder: (BuildContext context) => PackedScreen(
+           //  liste: album,
+         ),
+       ),
      );
    }
    else if(album.title=="Dispatch")
    {
-     Fluttertoast.showToast(
-       msg: "Dispatch",
-       textColor: Colors.white,
-       toastLength: Toast.LENGTH_SHORT,
-       timeInSecForIosWeb: 1,
-       gravity: ToastGravity.BOTTOM,
-       backgroundColor: Colors.indigo,
+     Navigator.push(
+       context,
+       MaterialPageRoute(
+         fullscreenDialog: true,
+         builder: (BuildContext context) => DispatchedScreen(
+           //  liste: album,
+         ),
+       ),
      );
    }
    else if(album.title=="Delivered")
    {
-     Fluttertoast.showToast(
-       msg: "Delivered",
-       textColor: Colors.white,
-       toastLength: Toast.LENGTH_SHORT,
-       timeInSecForIosWeb: 1,
-       gravity: ToastGravity.BOTTOM,
-       backgroundColor: Colors.indigo,
+     Navigator.push(
+       context,
+       MaterialPageRoute(
+         fullscreenDialog: true,
+         builder: (BuildContext context) => DeliveryScreen(
+           //  liste: album,
+         ),
+       ),
      );
    }
   }
